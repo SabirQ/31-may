4f8316b70338dfd6ea589280f8afae9a8adc164e
SabirQ <sabirmg@code.edu.az>
2022-06-01 02:36:34 +0400

    Practice Example added

---
+++ WebApplication2/WebApplication2/Controllers/DetailsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class DetailsController:Controller
+    {
+        public ActionResult product(int? Id)
+        {
+            if (Id==null)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ViewResult Index()
+        {
+            ViewResult view = new ViewResult();
+            view.ViewName = "HomePage";
+            return view;
+        }
+        public ViewResult About()
+        {
+            return new ViewResult { ViewName = "About" };
+        }
+        public ActionResult shop()
+        {
+
+            return View();
+        }
+    }
+}
